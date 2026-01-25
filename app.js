af8a7291f0087e9e8b4a74517c6b3c97e952c8a2
Бачище Назар Сергеевич <mcarim@mail.ru>
2026-01-25 22:51:32 +0300

    add few tabs

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const tabs = document.querySelectorAll(".tab");
 const blocks = document.querySelectorAll(".queue-block");
+const forms = document.querySelectorAll(".form-block");
+
 
 tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        tabs.forEach(t => t.classList.remove("active"));
-        tab.classList.add("active");
+tab.addEventListener("click", () => {
+// Активная вкладка
+tabs.forEach(t => t.classList.remove("active"));
+tab.classList.add("active");
 
-        const target = tab.dataset.target;
-        blocks.forEach(block => {
-            block.classList.toggle("active", block.id === target);
-        });
-    });
+
+const target = tab.dataset.target;
+
+
+// Переключаем блок очереди
+blocks.forEach(block => {
+block.classList.toggle("active", block.id === target);
 });
 
+
+// Переключаем форму
+forms.forEach(form => {
+form.classList.toggle("active", form.classList.contains(target));
+});
+});
+});
 const socket = new SockJS("http://localhost:8080/ws");
 const stompClient = Stomp.over(socket);
 
@@ -39,29 +51,68 @@ async function fetchQueuesSequentially() {
             const block = document.getElementById(queue);
             block.querySelector(".current").textContent = data.current ?? "-";
             block.querySelector(".next").textContent = data.next ?? "-";
+
         } catch(err) {
             console.error(`Error fetching queue ${queue}:`, err);
         }
     }
 }
 
+
 document.querySelectorAll(".next-btn").forEach(btn => {
     btn.addEventListener("click", async (e) => {
         const block = e.target.closest(".queue-block");
         const queue = block.id;
 
-        try {
-            const res = await fetch(`http://localhost:8080/api/v0.1/queue/${queue}/next`, {
-                method: "POST"
-            });
-            const data = await res.json();
-            block.querySelector(".current").textContent = data.current ?? "-";
-            block.querySelector(".next").textContent = data.next ?? "-";
-        } catch(err) {
-            console.error(`Error moving queue ${queue}:`, err);
-        }
+        $.ajax(
+            {
+            url: 'http://localhost:8080/api/v0.1/queue/'+ queue + '/next',
+            method: "POST",
+            success: function(response) {
+                $(".current").text(response.current)
+                $(".next").text(response.next)
+                $("#"+queue + "TicketNumber").val(response.current)
+            }
+            }
+        )
     });
 });
 
 
 fetchQueuesSequentially();
+
+$("#clientForm").on("submit", function (e) {
+e.preventDefault();
+
+
+const client = {
+fullName: $("#fullName").val(),
+height: Number($("#height").val()),
+weight: Number($("#weight").val()),
+shoeSize: Number($("#shoeSize").val()),
+skillLevel: $("#skillLevel").val(),
+ticketNumber: Number($("#registrationTicketNumber").val())
+};
+
+
+$.ajax({
+url: "http://localhost:8080/api/v0.1/clients",
+method: "POST",
+contentType: "application/json",
+data: JSON.stringify(client),
+
+
+success: function (response) {
+console.log(response);
+
+
+$("#clientForm")[0].reset();
+},
+
+
+error: function (err) {
+console.error(err);
+alert("Ошибка");
+}
+});
+});
